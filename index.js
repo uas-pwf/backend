1bea94a251651c011ec3b1c636b11452e1f17569
thonybiersack <thony.djie@gmail.com>
2023-07-10 11:48:01 +0700

    koneksi database

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const bodyParser = require('body-parser')
 const userRoute = require('./src/routes/Users')
 
 const app = express()
+const PORT = 8080
 
 app.use(bodyParser. urlencoded({extended: false}))
 
 app.use('/users', userRoute)
+
+app.listen(PORT, () => {
+    console.log(`server listening to port http://localhost:${PORT}`)
+})
